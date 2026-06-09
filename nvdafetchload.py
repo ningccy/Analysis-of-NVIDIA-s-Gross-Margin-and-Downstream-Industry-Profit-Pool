0a7c038b212ee521a254d97612079b4fa1a25b7d
ningccy <nning611231@gmail.com>
2026-06-09 20:25:50 +0800

    Update nvdafetchload.py

--- nvdafetchload.py
+++ nvdafetchload.py
@@ -33,3 +33,39 @@ for col in required_columns.keys():
 
 df_cleaned = df.rename(columns = required_columns)
 df_cleaned['gross_profit'] = df_cleaned['revenue'] - df_cleaned['cogs']
+df_cleaned['ticker'] = 'NVDA'
+df_cleaned['company_name'] = 'NVIDIA Corporation'
+df_cleaned['tier'] = 'Upstream
+
+final_cols = ['ticker', 'company_name', 'tier', 'fiscal_quarter', 'revenue', 'cogs', 'gross_profit', 'operating_income']
+df_to_load = df_cleaned[final_cols].copy()
+
+amount_cols = ['revenue', 'cogs', 'gross_profit', 'operating_income']
+for col in amount_cols:
+    df_to_load[col] = (df_to_load[col] / 1_000_000).round(2)
+
+print("\n cleaning complete:")
+print(df_to_load)
+print(f"\n start write into TiDB database [{DB_NAME}]...")
+##--------------------------------------
+upsert_sql = """
+INSERT INTO supply_chain_financials 
+(ticker, company_name, tier, fiscal_quarter, revenue, cogs, gross_profit, operating_income)
+VALUES (:ticker, :company_name, :tier, :fiscal_quarter, :revenue, :cogs, :gross_profit, :operating_income)
+ON DUPLICATE KEY UPDATE
+    revenue = VALUES(revenue),
+    cogs = VALUES(cogs),
+    gross_profit = VALUES(gross_profit),
+    operating_income = VALUES(operating_income);
+"""
+
+records = df_to_load.to_dict(orient='records')
+with engine.begin() as connection:
+    for record in records:
+        for k, v in record.items():
+            if pd.isna(v):
+                record[k] = None
+        
+        connection.execute(text(upsert_sql), record)
+
+print("complete! backend data is ready!")
