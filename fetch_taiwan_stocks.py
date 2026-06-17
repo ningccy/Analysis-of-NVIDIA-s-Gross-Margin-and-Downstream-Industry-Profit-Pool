28890dc689df6cf1fac5fdfa5afbca75741edb62
ningccy <nning611231@gmail.com>
2026-06-17 16:42:03 +0800

    Update fetch_taiwan_stocks.py

--- fetch_taiwan_stocks.py
+++ fetch_taiwan_stocks.py
@@ -3,11 +3,11 @@ from FinMind.data import DataLoader
 from sqlalchemy import create_engine, text
 import requests
 
-DB_HOST = "gateway01.ap-northeast-1.prod.aws.tidbcloud.com"
-DB_PORT = "4000"
-DB_USER = "5KntqF8ZunMNnjz.root"
-DB_PASSWORD = "tYXheZ6gJz1HnhV9"
-DB_NAME = "industry_analysis" 
+HOST = "gateway01.ap-northeast-1.prod.aws.tidbcloud.com"
+PORT = "4000"
+USER = "5KntqF8ZunMNnjz.root"
+PASSWORD = "tYXheZ6gJz1HnhV9"
+DB_NAME = "industry_analysis"
 
 engine = create_engine(f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}")
 
